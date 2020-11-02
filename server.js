453dffdce825c2b564b218b8430c0de7cd81bf48
Aman Kumar <aman.kmr4@yahoo.com>
2020-11-02 23:48:43 +1100

    added the two routes into server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+require("./routes/api")(app);
+require("./routes/routes")(app);
+
 app.listen(PORT, function () {
     console.log("App listning on PORT:" + PORT)
 })
