5b3dfc0fd0c3345935259c8140a9721e7bb25597
Aman Kumar <aman.kmr4@yahoo.com>
2020-11-02 23:42:17 +1100

    added the notes post function

--- routes/api.js
+++ routes/api.js
@@ -8,4 +8,26 @@ module.exports = (app) => {
     app.get("/api/notes", function (req, res) {
         res.json(dbData);
     });
+
+    app.post("/api/notes", function (req, res) {
+
+        if (dbData.length == 0) {
+            req.body.id = "0";
+        } else {
+            req.body.id = JSON.stringify(JSON.parse(dbData.length + 1));
+        }
+
+        dbData.push(req.body)
+        writeDB(dbData)
+        res.json(req.body);
+    });
+
+    function writeDB(dbData) {
+        dbData = JSON.stringify(dbData)
+        fs.writeFileSync("./db/db.json", dbData, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        })
+    };
 }
